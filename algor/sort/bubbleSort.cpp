faaa475bf8c0e05f93d8347cea7425fbe685b0e1
Midstream77 <73460097+Midstream77@users.noreply.github.com>
2021-05-15 22:35:21 +0800

    bubbleSort

--- algor/sort/bubbleSort.cpp
+++ algor/sort/bubbleSort.cpp
@@ -9,12 +9,18 @@ void swap(int &a, int&b){
 
 void bubble_sort(int* arr,int n){
     for(int i=0;i<n;i++){
-        for(int j=0;j<i;j++){
-
-        }
+        for(int j=1;j<n;j++)
+            if(arr[j-1]>arr[j]) swap(arr[j-1],arr[j]);
     }
 }
 
 int main(){
-    cout<<"test";
+    int n;
+    scanf("%d",&n);
+    int *arr = new int[n];
+    for(int i=0;i<n;i++) cin>>arr[i];
+    for(int i=0;i<n;i++) cout<<arr[i]<<"\t";
+    cout<<"\n";
+    bubble_sort(arr,n);
+    for(int i=0;i<n;i++) cout<<arr[i]<<"\t";
 }
