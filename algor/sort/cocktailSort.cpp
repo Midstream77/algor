0e3d40bdd65bf3eaf1ace8315eb0861144a51eaf
Midstream77 <73460097+Midstream77@users.noreply.github.com>
2021-05-15 22:35:48 +0800

    cocktailSort

---
+++ algor/sort/cocktailSort.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+void swap(int &a, int&b){
+    int t=a;
+    a=b;
+    b=t;
+}
+
+void cocktail_sort(int* arr,int n){
+    int start=1,end=n-1,step=1;
+    int left=1,right=n-1;
+    int i=0;
+    while(left!=right){
+        i+=step;
+        //cout<<"i="<<i<<"\t";
+        if(arr[i]<arr[i-1]) swap(arr[i],arr[i-1]);
+        if(i==end){
+            if(step==1){
+                right--;
+                end=left+1;
+                //cout<<"end="<<end<<endl;
+            }
+            if(step==-1){
+                left++;
+                end=right;
+                //cout<<"end="<<end<<endl;
+            }
+            step*=-1;
+        }
+    }
+}
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    int *arr = new int[n];
+    for(int i=0;i<n;i++) cin>>arr[i];
+    for(int i=0;i<n;i++) cout<<arr[i]<<"\t";
+    cout<<"\n";
+    cocktail_sort(arr,n);
+    for(int i=0;i<n;i++) cout<<arr[i]<<"\t";
+}
